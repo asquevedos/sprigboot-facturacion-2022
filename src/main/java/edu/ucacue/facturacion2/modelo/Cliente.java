91cae8fc61c987a2c77fcc58e2dfdd8662f5b4b9
asquevedos <squevedo8524@gmail.com>
2022-01-28 18:33:51 -0500

    Cambios necesarios para el sistema de Facturacion

--- src/main/java/edu/ucacue/facturacion2/modelo/Cliente.java
+++ src/main/java/edu/ucacue/facturacion2/modelo/Cliente.java
@@ -1,5 +1,6 @@
 package edu.ucacue.facturacion2.modelo;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,67 +8,98 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="clientes")
-public class Cliente {
+@Table(name="personas")
+public class Persona {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
-	
+	@Column(length = 30)
 	private String nombre;
+	@Column(length = 40)
 	private String apellido;
+	
+	@Column(unique = true, nullable = false)
+	private String cedula;
+	
 	private String direccion;
-	private String telefono;
-
-	public Cliente() {}
-	public Cliente(int id, String nombre, String apellido, String direccion, String telefono) {
+		
+	
+	public Persona(int id, String nombre, String apellido, String cedula) {
 		super();
 		this.id = id;
 		this.nombre = nombre;
 		this.apellido = apellido;
-		this.direccion = direccion;
-		this.telefono = telefono;
+		this.cedula = cedula;
 	}
+	
+	
+	
+	
+	public Persona(String nombre, String apellido, String cedula, String direccion) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.cedula = cedula;
+		this.direccion = direccion;
+	}
+
+
+
+
+	public Persona() {
+		super();
+	}
+
+
+
 
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getNombre() {
 		return nombre;
 	}
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
 	public String getApellido() {
 		return apellido;
 	}
-
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
 
 	public String getDireccion() {
 		return direccion;
 	}
 
+
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
 
-	public String getTelefono() {
-		return telefono;
-	}
 
-	public void setTelefono(String telefono) {
-		this.telefono = telefono;
+
+
+	@Override
+	public String toString() {
+		return "Persona [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", cedula=" + cedula
+				+ ", direccion=" + direccion + "]\n";
 	}
+	
+	
+	
 
 }
